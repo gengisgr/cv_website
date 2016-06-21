97cf6ef46ea32f6709b80ea3bd57bea819699fd5
gengisgr <gengisgr@gmail.com>
2016-06-21 00:12:10 -0600

    listo

--- www/contacto.php
+++ www/contacto.php
@@ -1,17 +1,18 @@
 <?php
 
-    $name = $_POST['nombre'];
-    $email = $_POST['email'];
-    $message = $_POST['mensaje'];
-    $from = 'From: gengis.info'; 
-    $to = 'gengisgr@gmail.com'; 
-    $subject = 'Mensaje desde gengis.info';
+    $name = $_POST["nombre"];
+    $email = $_POST["email"];
+    $message = $_POST["mensaje"];
+    $from = "From: gengis.info"; 
+    $to = "admin@gengis.info"; 
+    $subject = "Mensaje desde gengis.info";
+    $body =  "Nombre: $name\n E-Mail: $email\n Mensaje: $message";
 
-if ($_POST['submit']) {
+if ($_POST["submit"]) {
     if (mail ($to, $subject, $body, $from)) { 
-        echo '<p>¡Gracias, pronto te reponderé!</p>';
+        echo "<p style=text-align: center>¡Gracias, pronto me pondré en contacto!</p>";
     } else { 
-        echo '<p>Lo siento, No ha sido posible enviar tu mensaje</p>'; 
+        echo "<p style=text-align: center>Lo siento, no ha sido posible enviar tu mensaje</p>"; 
     }
 }
 
